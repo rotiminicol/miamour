d01f5f5a4735fb2be6e877ad851e71a9dcc8f509
Your Name <rotiminicol117@gmail.com>
2025-03-19 22:56:24 -1100

    update

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://miamour-rotiminicol117gmailcoms-projects.vercel.app/api" : "/api";
 
 export const axiosInstance = axios.create({
 	baseURL: BASE_URL,

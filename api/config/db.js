32b3b946aa3e96393197e86d34b2c01dd41e14b5
Your Name <rotiminicol117@gmail.com>
2025-04-02 03:34:16 -1100

    update

--- api/config/db.js
+++ api/config/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
    try{
-      const conn = await mongoose.connect(process.env.MONGO_URL);
-      console.log(`MongoDB connected: ${conn.connection.host}`);
+      const conn = await mongoose.connect(process.env.MONGO_URL, {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         serverSelectionTimeoutMS: 5000 // Timeout after 5 seconds
+       });
    } catch (error) {
       console.log("Error connecting to MongoDB: ", error);
       process.exit(1);
